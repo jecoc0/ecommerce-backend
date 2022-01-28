81d29db5572b0d54be400d21eae681633d33f8f0
Jessica <sevorg.jess@gmail.com>
2022-01-27 22:43:39 -0700

    set up models in sequelize

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -3,11 +3,12 @@ require('dotenv').config();
 // constructor so we can use new - create a new instance using this constructor
 const Sequelize = require('sequelize');
 
-const sequelize = process.env.JAWSDB_URL
+// this is where we are connecting to the database using Sequelize
 // shorthand for an if/else if JAWSDB_URL exists, 
 // use what is after the question mark
 // else, use what is after the colon
 // this is referred to as ternary operator
+const sequelizeConnection = process.env.JAWSDB_URL
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
@@ -17,4 +18,4 @@ const sequelize = process.env.JAWSDB_URL
       },
     });
 
-module.exports = sequelize;
+module.exports = sequelizeConnection;
--- Develop/models/ProductTag.js
+++ Develop/models/ProductTag.js
@@ -1,12 +1,33 @@
 const { Model, DataTypes } = require('sequelize');
 
 const sequelize = require('../config/connection');
+const Product = require('./Product');
 
 class ProductTag extends Model {}
 
 ProductTag.init(
   {
     // define columns
+    id: {
+      types: DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey: true,
+      autoIncrement: true
+    },
+    product_id: {
+      types: DataTypes.INTEGER,
+      references: {
+        model: Product,
+        key: 'id'
+      }
+    },
+    tag_id: {
+      types: DataTypes.INTEGER,
+      references: {
+        model: Tag,
+        key: 'id'
+      }
+    }
   },
   {
     sequelize,
