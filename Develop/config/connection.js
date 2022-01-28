d6566b575135c0df9a165ff0dd9d4ef80b008cd8
Jessica <sevorg.jess@gmail.com>
2022-01-27 22:24:59 -0700

    initial set up Category model

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -1,8 +1,13 @@
 require('dotenv').config();
 
+// constructor so we can use new - create a new instance using this constructor
 const Sequelize = require('sequelize');
 
 const sequelize = process.env.JAWSDB_URL
+// shorthand for an if/else if JAWSDB_URL exists, 
+// use what is after the question mark
+// else, use what is after the colon
+// this is referred to as ternary operator
   ? new Sequelize(process.env.JAWSDB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
       host: 'localhost',
