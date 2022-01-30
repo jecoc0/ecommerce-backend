d8d646c2049e80db516d633ffccb9b7ac7227359
Jessica <sevorg.jess@gmail.com>
2022-01-30 15:17:24 -0700

    added git routes for Category

--- models/index.js
+++ models/index.js
@@ -5,16 +5,27 @@ const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
 // Products belongsTo Category
+Product.belongsTo(Category)
+// getter is product.getCategory()
+
 
 // Categories have many Products
+Category.hasMany(Product)
+// getter will be category.getProducts()
+
 
 // Products belongToMany Tags (through ProductTag)
+Product.belongsToMany(Tag, { through: ProductTag });
+// getter will be product.getTags()
+
 
 // Tags belongToMany Products (through ProductTag)
+Tag.belongsToMany(Product, { through: ProductTag });
+// getter will be tag.getProducts()
 
 module.exports = {
   Product,
   Category,
-  // Tag,
-  // ProductTag,
+  Tag,
+  ProductTag,
 };
